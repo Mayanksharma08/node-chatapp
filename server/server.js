fbf7d6e0164c995942edcc38abe25915cda126ca
Mayank Sharma <cinegogwl@gmail.com>
2018-06-17 21:56:26 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage}=require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -15,18 +16,10 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
     //socket.emit from admin
-    socket.emit('newMessage',{
-        from:'Admin',
-        text:'Welcome tothe chat app',
-        createdAt:new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
     
     //bpradcast calll
-    socket.broadcast.emit('newMessage',{
-       from:'Admin',
-        text:'New User joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
     
 //    socket.emit('createMessage',{
 //        from: 'Kunal',
@@ -46,11 +39,7 @@ io.on('connection', (socket) => {
 //            text:message.text,
 //            createdAt:new Date().getTime()
 //        });
-        socket.broadcast.emit('newMessage',{
-            from: message.from,
-            text:message.text,
-            createdAt:new Date().getTime()
-        });
+        socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
     });
     
   socket.on('disconnect', () => {
